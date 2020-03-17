9136c6b46c792ee0c741ff70c96605b594a98862
Brittany Tinsley <brittanytinsley@Brittanys-MacBook-Pro.local>
2020-03-16 20:43:05 -0400

    First commit of project

---
+++ java.js
@@ -0,0 +1,13 @@
+// Must have greeting
+// first name and last name
+// console log result
+
+function myFavm(){
+    let favMovie = "My favorite movie of all time is Black Panther. ";
+    let favChara = "My favorite character is Okoye";
+    let actor = "Played by Danai Gurira";
+
+    let msg = favMovie.concat(`${favChara} ${actor}`);
+    alert(msg);
+
+}
